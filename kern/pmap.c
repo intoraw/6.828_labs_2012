13464dd00094a90525f429755c21a9bb580faead
zorksylar <pin.gao2008@gmail.com>
2013-08-19 20:31:33 +0800

    Lab3 PartB Full Score.

--- kern/pmap.c
+++ kern/pmap.c
@@ -591,7 +591,7 @@ user_mem_check(struct Env *env, const void *va, size_t len, int perm)
 	// LAB 3: Your code here.
   void * va_end = (void*)ROUNDUP(va + len, PGSIZE);
   pte_t * pte;
-  for ( ; va < va_end ; va += PGSIZE) {
+  for ( ; va < va_end ; va = ROUNDUP(va + PGSIZE, PGSIZE)) {
     pte = pgdir_walk(env->env_pgdir, va, 0);
     if (!pte || ( (*pte & (perm | PTE_P)) != (perm | PTE_P) )) {
       user_mem_check_addr = (uintptr_t)va;
